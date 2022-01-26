e426337a6736e976c4f2af5cfff361385f6d916b
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-01-26 14:19:22 -0300

    renderizando a pagina HTML para exibir POST completo

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +21,14 @@ public class CodeblogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
